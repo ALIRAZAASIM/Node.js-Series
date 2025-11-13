2921a66e5bdd24d4f301437c2cccb9e6348dcc65
M ALI RAZA <casim9530@gmail.com>
2025-11-14 04:23:10 +0500

    click

--- server/index.js
+++ server/index.js
@@ -2,15 +2,24 @@ const http = require("http");
 
 const fs = require("fs");
 
+const url = require("url");
+
 const myServer = http.createServer((req, res) => {
-  const log = `${req.url}:new request recieved\n`;
+  const log = `${req.url} new request recieved\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, () => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("hello you are on home page.");
         break;
       case "/about":
-        res.end("you are on about page");
+        const username = myUrl.query.myname;
+        res.end(`hi ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_qurry;
+        res.end(`here areyour search,${search}`);
       default:
         res.end("404 error");
     }
